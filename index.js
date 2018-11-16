5e69041e93e6ca51bf783259fae2668f4557846c
Mik3Dev <miguel.acosta1978@gmail.com>
2018-11-15 22:57:07 -0300

    Add socket features

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+const io = socketIO(server);
 const logger = require('morgan');
 var proxy = require('express-http-proxy');
 
@@ -10,6 +14,7 @@ const port = process.env.PORT || config.PORT;
 const Candle = require('./commons/candleObject');
 const _ = require('lodash');
 const mongoose = require('mongoose');
+const candleModel = require('./models/candle');
 const timeframes = require('./commons/timeFrames');
 const instruments = require('./commons/instruments');
 
@@ -24,10 +29,36 @@ mongoose.connect(process.env.DATABASE_URL, {
     err => console.log('Ups, something went wrong. Can not connect to database.')
 );
 
-app.use('/', proxy(config.proxy));
+app.get('/stats', (req, res) => {
+    const counts = {};
+    candleModel.count({'timeframe': 'S30'}).then(count => {
+        counts.S30 = count;
+        candleModel.count({'timeframe': 'M5'}).then(count => {
+            counts.M5 = count;
+            candleModel.count({'timeframe': 'M15'}).then(count => {
+                counts.M15 = count;
+                candleModel.count({'timeframe': 'H1'}).then(count => {
+                    counts.H1 = count;
+                    res.send(counts);
+                });
+            });
+        });
+    }).catch(e => {
+        counts='Ups! Something went wrong. Contact me miguel.acosta1978@gmail.com';
+        res.send(counts);
+    });
+})
+
+app.use('/app', proxy(config.proxy));
 
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    setInterval( () => {
+        socket.emit('Message', 'Hello World')
+    }, 60000)
+});
+
+server.listen(port, () => {
     console.log(`The app is running on port: ${port}`);
     setInterval(() => {
         _.forEach(instruments, instrument => {
